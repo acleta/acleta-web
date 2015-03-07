ea77ea3149151652f4b7784aeda8618c8785c1c2
Seba Gamboa <me@sagmor.com>
2015-03-07 17:17:31 -0300

    Fix jshint error

--- app/pods/map/layers/icon_marker.js
+++ app/pods/map/layers/icon_marker.js
@@ -1,12 +1,12 @@
 import {faIcon} from 'acleta/helpers/fa-icon';
 /* global L */
 
-export default function(icon, location) {
+export default function(iconName, location) {
   var icon = L.divIcon({
     className: '',
-    html : faIcon(icon, {hash: {}})
+    html : faIcon(iconName, {hash: {}})
   });
   var marker = new L.Marker(location, {icon: icon});
 
   return marker;
-};
+}
