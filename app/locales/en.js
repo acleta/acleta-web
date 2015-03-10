bbcfa5977fda0002d90b155521dc842b1be77eb9
Seba Gamboa <me@sagmor.com>
2015-03-10 17:36:52 -0300

    Add missing translations

--- app/locales/en.js
+++ app/locales/en.js
@@ -1,3 +1,18 @@
 export default {
-  back: 'Back'
+  back: 'Back',
+  address: 'Address',
+  status: 'Status',
+  close: 'close',
+  about_us: 'About us',
+  bike_santiago: {
+    statuses: {
+      active: 'Active',
+      closed: 'Closed',
+      empty: 'Without bikes',
+      full: 'Without docks',
+      unavailable: 'Unavailable',
+      unknown: 'Unknown'
+    }
+  }
 };
+
